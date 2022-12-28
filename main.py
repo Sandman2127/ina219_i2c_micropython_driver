d2d671b4d9059f9ab8400e5e628a81a4728ddead
Dean <sandman2127@gmail.com>
2022-12-28 05:05:49 -0600

    significant rewrite using OOP

--- main.py
+++ main.py
@@ -1,36 +1,12 @@
 # use micropython
 
-import random
+from random import randint
 import time
-import machine
+from machine import I2C,Pin
 import ssd1306
-from ina219 import INA219  # micropython version: https://github.com/chrisb2/pyb_ina219
+from micropython import const
 
-# general i2c stuff 
-# print('Scan i2c bus...')
-#devices = i2c.scan()
-
-#if len(devices) == 0:
-#    print("No i2c device !")
-#else:
-#    print('i2c devices found:',len(devices))
-
-# for device in devices:
-#print("Decimal address: ",device," | Hexa address: ",hex(device))
-#
-
-# perform setup of globals
-# screen
-sda_screen = machine.Pin(0)
-scl_screen = machine.Pin(1)
-i2c_screen = machine.I2C(0,sda=sda_screen,scl=scl_screen,freq=400000)
-display = ssd1306.SSD1306_I2C(128, 64, i2c_screen)
-
-# ina219
-sda_sensor = machine.Pin(0)
-scl_sensor = machine.Pin(1)
-i2c_sensor = machine.I2C(0,sda=sda_sensor,scl=scl_sensor,freq=400000)
-
+                            
 def rewrite_display(voltage,current,power,prev_data):
     #https://docs.micropython.org/en/latest/esp8266/tutorial/ssd1306.html
     # overwrite the old stuff
@@ -53,38 +29,148 @@ def write_display_nonchanging_sections():
     display.text('Voltage:', 5, 24, 1)
     display.text('Current:', 5, 48, 1)
     display.show()
+    
+def convert_v_byte():
+    pass
+
+def convert_i_byte():
+    pass
+
+def convert_power_byte():
+    pass
+
+"""
+                    *** MAIN ***
+"""
+
+# define setup variables
+debug = 0 
+# perform setup of globals
+# screen
+sda_screen = machine.Pin(0)
+scl_screen = machine.Pin(1)
+i2c_screen = I2C(0,sda=sda_screen,scl=scl_screen,freq=400000)
+display = ssd1306.SSD1306_I2C(128, 64, i2c_screen)
+
+# ina219
+sda_sensor = machine.Pin(0)
+scl_sensor = machine.Pin(1)
+i2c_sensor = machine.I2C(0,sda=sda_sensor,scl=scl_sensor,freq=400000)
+config_address = const(0)
+shunt_voltage_address = const(1)
+voltage_address = const(2)
+current_address = const(3)
+power_address = const(4)         
+devices = i2c_screen.scan()
 
 ### for debug only
-#class INA219:
-#     def __init__(self):
-#         self.voltage = 0.100
-#         self.current = 0.219
-#         self.power = self.voltage * self.current
-        
-#     def fluctuate_voltage(self,input):
-#         self.voltage = self.voltage + input
-#         self.power = self.voltage * self.current
-        
-#     def fluctuate_current(self,input):
-#         self.current = self.current + input
-#         self.power = self.voltage * self.current
-#ina = INA219()
-#randomlist = []
-#for i in range(0,1000):
-#     n = random.randint(-1,1)
-#     randomlist.append(n)
+if debug == 1:
+    class INA219:
+         def __init__(self):
+             self.voltage = 0.100
+             self.current = 0.219
+             self.power = self.voltage * self.current
+            
+         def fluctuate_voltage(self,input):
+             self.voltage = self.voltage + input
+             self.power = self.voltage * self.current
+            
+         def fluctuate_current(self,input):
+             self.current = self.current + input
+             self.power = self.voltage * self.current
+    ina = INA219()
+    randomlist = []
+    for i in range(0,1000):
+         n = randint(-1,1)
+         randomlist.append(n)
+else:
+    class INA219:
+        def __init__(self,address_in):
+            self.peripheral_address = address_in
+            self.voltage = 0
+            self.current = 0
+            self.power = 0 
+            # initiate communication with the device
+            startup_buffer = bytearray(0x399F)  # '00111001 10011111' 
+            # 0 is the configuration address 
+            i2c_sensor.writeto_mem(self.peripheral_address,config_address,startup_buffer)
+            
+        def change_mem_address(device,mem_address):
+            i2c_sensor.writeto(device, b'123') 
 
-# setup ina219 
-SHUNT_OHMS = 0.1  # Check value of shunt used with your INA219
-ina = INA219(SHUNT_OHMS,i2c_sensor)
-I2C_INTERFACE_NO = 0
-ina = INA219(SHUNT_OHMS,I2C(I2C_INTERFACE_NO))
-ina.configure()
+        def get_voltage():
+            voltage_byte = i2c_sensor.readfrom_mem(self.peripheral_address,voltage_address, 2) # read 2 bytes from the voltage mem address from the peripheral device
+            self.voltage = convert_v_byte(voltage_byte)
+            time.sleep_ms(1)
+        def get_current():
+            current_byte = i2c_sensor.readfrom_mem(self.peripheral_address,current_address, 2) # read 2 bytes from the current mem address from the peripheral device
+            self.current = convert_i_byte(current_byte)
+            time.sleep_ms(1)
+        def get_power():
+            power_byte = i2c_sensor.readfrom_mem(self.address,power_address, 2)  # read 2 bytes from the current mem address from the peripheral device
+            self.power = convert_power_byte(power_byte)
+            time.sleep_ms(1)
+
+        """
+            I2C operations: https://docs.micropython.org/en/latest/library/machine.I2C.html
+
+            i2c = I2C(freq=400000)          # create I2C peripheral at frequency of 400kHz
+                                            # depending on the port, extra parameters may be required
+                                            # to select the peripheral and/or pins to use
+
+            i2c.scan()                      # scan for peripherals, returning a list of 7-bit addresses
+
+            i2c.writeto(42, b'123')         # write 3 bytes to peripheral with 7-bit address 42
+            i2c.readfrom(42, 4)             # read 4 bytes from peripheral with 7-bit address 42
+
+            i2c.readfrom_mem(42, 8, 3)      # read 3 bytes from memory of peripheral 42,
+                                            #   starting at memory-address 8 in the peripheral
+            i2c.writeto_mem(42, 2, b'\x10') # write 1 byte to memory of peripheral 42
+                                            #   starting at address 2 in the peripheral
+            Memory operations¶
+            Some I2C devices act as a memory device (or set of registers) that can be read from and written to.
+            In this case there are two addresses associated with an I2C transaction: the peripheral address and the memory address.
+            The following methods are convenience functions to communicate with such devices.
+
+            I2C.readfrom_mem(addr, memaddr, nbytes, *, addrsize=8)
+                
+            Read nbytes from the peripheral specified by addr starting from the memory address specified by memaddr.
+            The argument addrsize specifies the address size in bits. Returns a bytes object with the data read.
+
+            I2C.readfrom_mem_into(addr, memaddr, buf, *, addrsize=8)
+                
+            Read into buf from the peripheral specified by addr starting from the memory address specified by memaddr.
+            The number of bytes read is the length of buf. The argument addrsize specifies the address size in bits (on ESP8266 this argument is not recognised and the address size is always 8 bits).
+
+            The method returns None.
+
+            I2C.writeto_mem(addr, memaddr, buf, *, addrsize=8)
+                
+            Write buf to the peripheral specified by addr starting from the memory address specified by memaddr.
+            The argument addrsize specifies the address size in bits (on ESP8266 this argument is not recognised and the address size is always 8 bits).
+
+            The method returns None.
+        """
+            
+    # setup ina219
+    SHUNT_OHMS = 0.1 
+    ina = INA219(devices[1]) # should be 2nd i2c device i.e. the ina219
+    
 # write displays never changing functions 
 write_display_nonchanging_sections()
 # measure and display loop
 run_loop = 1
 passed_rnd_1 = False
+
+
+if len(devices) == 0:
+     print("No i2c device !")
+else:
+     print('found:',len(devices),'i2c devices')
+     cnt = 0
+     for dev in devices:
+         print("device:",cnt,dev,hex(dev))
+         cnt += 1
 
 while True:
     ### for the micropython  ### 
@@ -102,7 +188,8 @@ while True:
         else:
             prev_data = rewrite_display(ina.voltage,ina.current,ina.power,[0,0,0])
             passed_rnd_1 = True
-        time.sleep_ms(200)
+        time.sleep_ms(1000)
+    time.sleep_ms(1000)
 
 
 # circuit python version
